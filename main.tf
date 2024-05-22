2f23f57b1f9e657050048bd154b3c4bc763a9883
Miranda_Reyes_HKKD <143029234+mirly14@users.noreply.github.com>
2024-05-22 14:59:44 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,13 @@
-/* resource "snowflake_warehouse" "demo" {
-  name = "demo_warehouse"
-  warehouse_size = "Large"
-  auto_suspend   = 60
-}
+# ***
 
-resource "snowflake_database" "simple" {
-  name = "testing"
-  comment = "test comment"
-  data_retention_time_in_days = 3
+# This is a terraform provider plugin for managing Snowflake accounts
+
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "Snowflake-Labs/snowflake" # This variable does not change. It is unique for the Snowflake plugin
+      version = "~> 0.90.0" # This is the most recent version of the plugin
+    }
+  }
 }
- */
-# Privileges
 

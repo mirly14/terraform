07ece71b434cfd2e99f1ed8d35ec5d241fa7aa18
Jonathan_Sanchez_HKKD <143029087+Jsanchez-1201@users.noreply.github.com>
2024-05-22 14:51:46 -0600

    Update providers.tf
    edited file

--- providers.tf
+++ providers.tf
@@ -1,15 +1,30 @@
-/* terraform {
-  required_providers {
-    snowflake = {
-      source  = "Snowflake-Labs/snowflake"
-      version = "~> 0.76"
-    }
-  }
+# -- Providers -- #
+
+# Here goes the credentials to connect to your Snowflake account
+# It is a best practice to write any name with capitalize letters (e.g. user name, schema name, and so on)
+
+provider "snowflake" {
+  user = "USER_ACCOUNT" # *required*
+  password = "Abc12345"  # *required*
+  account  = "EQB69117" # *required* ~ This is the account locator number in Snowflake
+  role = "SYSADMIN"
+  region = "us-east-1" # Only necessary for this element
 }
 
 provider "snowflake" {
-  user = var.snowflake_username
-  password = var.snowflake_password
-  account  = var.snowflake_account_name
-  region   = var.snowflake_region
-} */
+  alias = "orgadmin"
+  user = "USER_ACCOUNT" # *required*
+  password = "Abc12345" # *required*
+  account  = "EQB69117" # *required* 
+  role = "ORGADMIN"
+  region = "us-east-1" 
+}
+
+provider "snowflake" {
+  alias = "accountadmin" # You can specify an alias for each to differentiate (securityadmin | sysadmin | public | custom_roles )
+  user = "USER_ACCOUNT" # *required*
+  password = "Abc12345"  # *required*
+  account  = "EQB69117" # *required*
+  role = "ACCOUNTADMIN"
+  region = "us-east-1"
+}  

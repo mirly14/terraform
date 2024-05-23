108aa04fd337f89eb0eec82ab298355c8bfeef49
Miranda_Reyes_HKKD <143029234+mirly14@users.noreply.github.com>
2024-05-23 09:36:39 -0600

    Create storage_integration.tf

---
+++ storage_integration.tf
@@ -0,0 +1,20 @@
+# -- Storage Integration -- #
+
+# resource "snowflake_storage_integration" "integration" {
+#   name    = "storage" # *required*
+#   comment = "A storage integration."
+#   type    = "EXTERNAL_STAGE" # Accepted values EXTERNAL_STAGE | INTERNAL_STAGE
+
+#   enabled = true # Accepted values true | false
+
+#   storage_allowed_locations = [""] #*required*
+#   storage_blocked_locations = [""] # Explicitly prohibits external stages that use the integration from referencing one or more storage locations
+#   storage_aws_object_acl    = "bucket-owner-full-control" # Enables support for AWS access control lists (ACLs) to grant the bucket owner full control
+
+#   storage_provider         = "S3" # *required*
+#   storage_aws_external_id  = "..." # The external ID that Snowflake will use when assuming the AWS role
+#   storage_aws_iam_user_arn = "..." # he Snowflake user that will attempt to assume the AWS role
+#   storage_aws_role_arn     = "..." # 
+  
+#   # azure_tenant_id  
+# }
